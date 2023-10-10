217e1fb1bc58cb45ada18c22435242d175d65337
Luis Manuel Brito Coelho <98318766+louisrabit@users.noreply.github.com>
2023-10-10 23:17:45 +0100

    ApiFilmClassTestPostman

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -1,8 +1,35 @@
-﻿namespace MovieApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
 
-public class MovieController
+namespace MovieApi.Controllers;
+
+
+// Para que seja um controlador e esteja abil a receber requesiçoes 
+// 1 : Anotaçoes :
+[ApiController]
+//1.1 Anotaçoes de rota 
+//rota tem que ser para o nome do controlador ( controller )
+[Route("[controller]")]
+
+// 1.3 : temos que extender , ControllerBase
+public class MovieController : ControllerBase
 {
+    [HttpPost]//4:  vamos fazer uma Postagem do filme 
+    // 2 : Metodo para registar um filme 
+    // Para adicionar o filme no Sistema mas temos que receber alguma informaçao, via parametro
+    // iremos receber um filme como parametro
+    public void AddMovie(Filme filme)
+    {
+        // vamos ter que adicionar o filme a uma lista 
+        filmes.Add(filme);
+        Console.WriteLine(filme.Title);
+        Console.WriteLine(filme.Time);
+    }
+
+    // 3: Temos que criar : Lista 
+    // temos que criar o objecto --> classe que representa , filme no programa
+    private static List<Filme> filmes = new List<Filme>();
 
 
-
+   
 }
---
+++ MovieApi/Models/FilmeClass.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieApi.Models;
+
+public class Filme
+{
+    public Filme(string title, int time, string gender)
+    {
+        Title = title;
+        Time = time;
+        Gender = gender;
+    }
+
+    public string Title { get; set; }    
+    public int Time { get; set; }
+    public string Gender { get; set; }
+
+}
