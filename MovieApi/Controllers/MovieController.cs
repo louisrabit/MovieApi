67460fd2a5aebfcf104ce9340e425c44a4637fc3
Luis Manuel Brito Coelho <98318766+louisrabit@users.noreply.github.com>
2023-10-11 11:45:48 +0100

    MethodsGetAndPost

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -15,21 +15,43 @@ namespace MovieApi.Controllers;
 public class MovieController : ControllerBase
 {
     [HttpPost]//4:  vamos fazer uma Postagem do filme 
+    //5 : Neste momento fizemos um Post no Postman , com url : 7078/Movie 
+    //5: passamos no body um Json , com o titulo , genero e time e deu codigo 200, ok
+    // --> na consola apareve o codigo 200 e ALURA Movie
+    // Neste momento passamos um filme  na nossa lista 
+
     // 2 : Metodo para registar um filme 
     // Para adicionar o filme no Sistema mas temos que receber alguma informaçao, via parametro
     // iremos receber um filme como parametro
-    public void AddMovie(Filme filme)
+    public void AddMovie([FromBody] Filme filme)
     {
+
+        // ESTA SERIA UMA POSSIBILIDADE PARA POR CERTOS LIMITES E VALLIDAÇOES , PARA RESPEITE CERTAS REGRAS
+        // Mas existe uma melhor maneira --> data notations !! Por os requesitros em cima dos campos da classe
+
+        //if (
+        //    !string.IsNullOrEmpty(filme.Title) && filme.Title.Length <= 300 &&
+        //        !string.IsNullOrEmpty(filme.Gender) && filme.Time >= 25
+        //        ) ;
+
+
         // vamos ter que adicionar o filme a uma lista 
         filmes.Add(filme);
         Console.WriteLine(filme.Title);
         Console.WriteLine(filme.Time);
     }
 
+
     // 3: Temos que criar : Lista 
     // temos que criar o objecto --> classe que representa , filme no programa
     private static List<Filme> filmes = new List<Filme>();
 
+    //Metodo de leitura
 
-   
+    [HttpGet]
+    public List<Filme> ReadListFIlms()
+    {
+        return filmes;
+    }
+
 }
--- MovieApi/Models/FilmeClass.cs
+++ MovieApi/Models/FilmeClass.cs
@@ -1,4 +1,6 @@
-﻿namespace MovieApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models;
 
 public class Filme
 {
@@ -9,8 +11,15 @@ public class Filme
         Gender = gender;
     }
 
-    public string Title { get; set; }    
+
+    // Data notations => Required + Error Message +  etc..
+    [Required(ErrorMessage = "The Title field is required.")]
+    [MaxLength(50, ErrorMessage = "Max Leght , 50 characters")]
+    public string Title { get; set; }
+    [Required(ErrorMessage = "The Time field is required. ")]
+    [Range(1, 400, ErrorMessage = "Movie need to have more than 1 minute and equal or less than 400 minuts")]
     public int Time { get; set; }
+    [Required(ErrorMessage = "The Gender field is required.")]
     public string Gender { get; set; }
 
 }
